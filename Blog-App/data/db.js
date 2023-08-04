a61a9dd3d9b3389c458f8f06cabeb58bffeee640
melihkaan240 <melihkaanarac@gmail.com>
2023-08-04 19:06:37 +0300

    connected mysql and reading db

---
+++ Blog-App/data/db.js
@@ -0,0 +1,14 @@
+// sql bağlantı kütüphanesi olan mysql2
+const mysql = require("mysql2")
+const config = require("../config")
+let connection = mysql.createConnection(config.db)
+
+connection.connect(function (err) {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log("mysql server bağlantısı yapıldı");
+})
+
+module.exports=connection.promise()
--- Blog-App/routes/user.js
+++ Blog-App/routes/user.js
@@ -1,47 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const path = require("path");
+const db = require("../data/db");
 
-const data = {
-    title:"Popüler Kurslar",
-    categories:["Web Geliştirme","Programlama","Mobil Uygulamalar","Veri Analizi"],
-    blogs:[
-      {
-        blogid:1,
-        title:"Komple Uygulamali Web Geliştirme",
-        description:"Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
-        image:"1.jpeg",
-        mainPage:true,
-        approved:true
-      },
-      {
-        blogid:2,
-        title:"Python ile Sıfırdan İleri Seviye Python Programlama",
-        description:"Sıfırdan İleri Seviye Python Dersleri.Veritabanı,Veri Analizi,Bot Yazımı,Web Geliştirme(Django)",
-        image:"2.jpeg",
-        mainPage:true,
-        approved:true
-      },
-      {
-        blogid:3,
-        title:"Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+",
-        description:"Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",
-        image:"3.jpeg",
-        mainPage:false,
-        approved:true
-      },
-        {
-          blogid:4,
-        title:"Node.js ile Sıfırdan İleri Seviye Web Geliştirme",
-        description:"Node.js ile sıfırdan ileri seviye dinamik web uygulaması geliştirmeyi öğren.",
-        image:"4.jpeg",
-        mainPage:false,
-        approved:true
-        }
-
-    ]
-}
 
 router.use("/blogs/:blogid", function (req, res) {
   res.render("users/blog-details");
@@ -51,13 +12,41 @@ router.use("/blogs/:blogid", function (req, res) {
 });
 
 router.use("/blogs", function (req, res) {
-  res.render("users/blogs",data);
+  db.execute("select * from blog where approved=1")
+  .then((result) => {
+    db.execute("select * from category")
+
+      .then((sonuc) => {
+
+        res.render("users/blogs", {
+          title: "Tüm Kurslar",
+          blogs: result[0],
+          categories: sonuc[0],
+        });
+      })
+      .catch((err) => console.log(err));
+  })
+  .catch((err) => console.log(err));
 
   // res.sendFile(path.join(__dirname,"../views/users","blogs.html"));
 });
 
 router.use("/", function (req, res) {
-  res.render("users/index",data);
+  db.execute("select * from blog where approved=1 and mainPage=1")
+    .then((result) => {
+      db.execute("select * from category")
+
+        .then((sonuc) => {
+
+          res.render("users/index", {
+            title: "Popüler Kurslar",
+            blogs: result[0],
+            categories: sonuc[0],
+          });
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
 
   // res.sendFile(path.join(__dirname,"../views/users","index.html"));
 });
