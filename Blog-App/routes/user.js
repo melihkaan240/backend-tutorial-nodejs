ba0babffe89ed567781f2d406e1104d2b37c4f7d
melihkaan240 <melihkaanarac@gmail.com>
2023-08-17 01:58:52 +0300

    created: blog detail

--- Blog-App/routes/user.js
+++ Blog-App/routes/user.js
@@ -4,51 +4,57 @@ const router = express.Router();
 const db = require("../data/db");
 
 
-router.use("/blogs/:blogid", function (req, res) {
-  res.render("users/blog-details");
+router.use("/blogs/:blogid",async function (req, res) {
+  const id = req.params.blogid;
+  try {
+    const [blog, ]=await db.execute("select * from blog where blogid=?",[id])
 
-  // path.join path yollarını birleştirir
-  //   res.sendFile(path.join(__dirname,"../views/users","blog-details.html"));
+    if(blog[0]){
+     return   res.render("users/blog-details",{
+        blog:blog[0]
+      }); 
+    }
+
+res.redirect("/")
+  } catch (error) {
+    console.log(error);
+  }
+
 });
 
-router.use("/blogs", function (req, res) {
-  db.execute("select * from blog where approved=1")
-  .then((result) => {
-    db.execute("select * from category")
+router.use("/blogs", async function (req, res) {
 
-      .then((sonuc) => {
+try{
+  const [blogs, ] = await db.execute("select * from blog where approved=1")
+  const [categories, ] = await db.execute("select * from category")
+  res.render("users/blogs", {
+    title: "Tüm Kurslar",
+    blogs: blogs,
+    categories: categories,
+  });
+}
+catch(err){
+console.log("this is your error:",err);
+}
 
-        res.render("users/blogs", {
-          title: "Tüm Kurslar",
-          blogs: result[0],
-          categories: sonuc[0],
-        });
-      })
-      .catch((err) => console.log(err));
-  })
-  .catch((err) => console.log(err));
-
-  // res.sendFile(path.join(__dirname,"../views/users","blogs.html"));
 });
 
-router.use("/", function (req, res) {
-  db.execute("select * from blog where approved=1 and mainPage=1")
-    .then((result) => {
-      db.execute("select * from category")
+router.use("/", async function (req, res) {
 
-        .then((sonuc) => {
+  try{
+    const [blogs, ] = await db.execute("select * from blog where approved=1 and mainPage=1")
+    const [categories,] = await db.execute("select * from category")
+    res.render("users/index", {
+      title: "Popüler Kurslar",
+      blogs: blogs,
+      categories: categories,
+    });
+     console.log(blogs);
+  }
+  catch(err){
+  console.log("this is your error:",err);
+  }
 
-          res.render("users/index", {
-            title: "Popüler Kurslar",
-            blogs: result[0],
-            categories: sonuc[0],
-          });
-        })
-        .catch((err) => console.log(err));
-    })
-    .catch((err) => console.log(err));
-
-  // res.sendFile(path.join(__dirname,"../views/users","index.html"));
 });
 
 module.exports = router;
