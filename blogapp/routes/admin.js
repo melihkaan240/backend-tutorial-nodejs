eebc7f5d4b9577e3e18ad376b6193f5520eaf079
melihkaan240 <melihkaanarac@gmail.com>
2023-08-25 03:26:17 +0300

    edit page finish

--- blogapp/routes/admin.js
+++ blogapp/routes/admin.js
@@ -34,18 +34,59 @@ router.post("/blog/create", async function(req, res) {
     }
 });
 
-router.get("/blogs/:blogid", function(req, res) {
-    res.render("admin/blog-edit");
+router.get("/blogs/:blogid", async function(req, res) {
+    const blogid = req.params.blogid;
+
+    try {
+        const [blogs, ] = await db.execute("select * from blog where blogid=?", [blogid]);
+        const [categories, ] = await db.execute("select * from category");
+        const blog = blogs[0];
+
+        if(blog) {
+            return res.render("admin/blog-edit", {
+                title: blog.baslik,
+                blog: blog,
+                categories: categories
+            });
+        }
+
+        res.redirect("admin/blogs");
+    }
+    catch(err) {
+        console.log(err);
+    }
+
 });
+
+
+router.post("/blogs/:blogid", async function(req,res){
+    const blogid = req.body.blogid;
+    const baslik = req.body.baslik;
+    const aciklama = req.body.aciklama;
+    const resim = req.body.resim;
+    const anasayfa = req.body.anasayfa=="on" ? 1 : 0;
+    const onay = req.body.onay=="on" ? 1 : 0;
+    const kategoriid = req.body.kategori;
+
+
+    try {
+        await db.execute("UPDATE blog SET baslik=?, aciklama=?, resim=?, anasayfa=?, onay=?, categoryid=? WHERE blogid=?",[baslik, aciklama, resim, anasayfa, onay, kategoriid, blogid])
+        res.redirect("/admin/blogs");
+    } catch (error) {
+        console.log(error);
+    }
+
+})
 
 router.get("/blogs", async function(req, res) {
     try {
-        const [blogs,]= await db.execute("select blogid,baslik,resim,onay from blog")
-        res.render("admin/blog-list",{
-            title:"blog list",
-            blogs:blogs
-        }) 
-    } catch (err) {
+        const [blogs,] = await db.execute("select blogid, baslik, resim from blog");
+        res.render("admin/blog-list", {
+            title: "blog list",
+            blogs: blogs
+        });
+    }
+    catch(err) {
         console.log(err);
     }
 });
